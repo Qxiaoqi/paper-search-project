a0bcd348a592f1120b1563ab3e863fce32f8fc0c
xiaoqi <308805548@qq.com>
2019-03-14 19:20:41 +0800

    准备拆分成多页应用前的提交

--- src/main.js
+++ src/main.js
@@ -8,8 +8,6 @@ import "normalize.css"; //css部分初始化
 import "font-awesome/css/font-awesome.css"; //icon
 import vMessage from "./components/message/index"; //封装的全局提示组件
 
-
-
 Vue.config.productionTip = false;
 Vue.prototype.$api = api;
 Vue.use(vMessage);
